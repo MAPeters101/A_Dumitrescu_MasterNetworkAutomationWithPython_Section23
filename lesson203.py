0b10d01a38e203a958246fc4cd86faf89957d4e4
Mark Peters <mapeters101@earthlink.net>
2024-11-19 00:17:20 -0500

    Lesson203:  Work on lesson.

---
+++ lesson203.py
@@ -0,0 +1,35 @@
+
+r = range(2, 10)
+print(type(r))
+print(r)
+print(list(r))
+print()
+
+
+r = range(10)
+print(type(r))
+print(r)
+print(list(r))
+print()
+
+
+r = range(2, 10, 3)
+print(type(r))
+print(r)
+print(list(r))
+print()
+
+print(list(range(0, 11, 2)))
+print(list(range(0, 40, 7)))
+print()
+
+s = sum(range(0, 1001))
+print(s)
+print()
+
+print(list(range(-20, 10, 4)))
+print(list(range(10, -2, -3)))
+
+
+
+
