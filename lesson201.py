4ad96e042590ac00e898b0c8e6a0179047e43683
Mark Peters <mapeters101@earthlink.net>
2024-11-18 23:53:00 -0500

    Lesson201:  Work on lesson to 4:30.

---
+++ lesson201.py
@@ -0,0 +1,16 @@
+
+for letter in 'Python':
+    print(letter)
+    print('bye')
+    print('########')
+
+for letter in [1, 2, 3]:
+    print(letter)
+
+for letter in (1, 2, 3):
+    print(letter)
+
+for letter in {1, 'a', 'b'}:
+    print(letter)
+
+
