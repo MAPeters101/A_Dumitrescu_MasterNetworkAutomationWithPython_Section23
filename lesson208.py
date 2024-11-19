3bea7c31f44e6178449bd49fd5e53feca98057ef
Mark Peters <mapeters101@earthlink.net>
2024-11-19 10:54:02 -0500

    Lesson208:  Work on lesson.

---
+++ lesson208.py
@@ -0,0 +1,20 @@
+# x = 12
+# while x < 100:
+#     if x%13 != 0:
+#         continue
+#     print(x)
+#     x += 1
+#
+x = 12
+while x < 100:
+    x += 1
+    if x%13 != 0:
+        continue
+    print(x)
+
+
+
+
+
+
+
