98207457bb01c511b03e0d7b870d7271711f920d
Mark Peters <mapeters101@earthlink.net>
2024-11-18 23:56:50 -0500

    Lesson201:  Complete work on lesson from 4:30.

--- lesson201.py
+++ lesson201.py
@@ -2,7 +2,7 @@
 for letter in 'Python':
     print(letter)
     print('bye')
-    print('########')
+print('########')
 
 for letter in [1, 2, 3]:
     print(letter)
@@ -12,5 +12,14 @@ for letter in (1, 2, 3):
 
 for letter in {1, 'a', 'b'}:
     print(letter)
+
+# for letter in 8:
+#     print(letter)
+
+my_str = input('Enter something: ')
+vowels = 'aeiou'
+for item in my_str:
+    if item in vowels:
+        print(item, end=' ')
 
 
